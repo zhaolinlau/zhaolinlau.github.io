2e275878003741f2972563b8c14d167e99faa494
Zhaolin Lau <zhaolinlau@gmail.com>
2023-01-03 02:21:13 +0800

    bump package version, add social media, add selfie photo

--- src/components/about.vue
+++ src/components/about.vue
@@ -1,11 +1,20 @@
 <template>
-  <section class="section has-text-centered" id="about">
+  <section class="section" id="about">
     <div class="container">
-      <p class="title mt-3">About Me</p>
-      <p class="subtitle">Jack of all trades, master of "some"</p>
-      <p>
-        Web developer with more than <b>3 years</b> of well-rounded experience with a diploma in the field of <b>Computer Science</b>, extensive knowledge of modern Web techniques and love for <b>Milo</b>. Looking for an opportunity to work and upgrade, as well as being involved in an organization that believes in gaining a competitive edge and giving back to the community.
-      </p>
+      <p class="title mt-3 has-text-centered">About Me</p>
+      <div class="columns is-multiline is-vcentered">
+        <div class="column is-4">
+          <figure class="image">
+            <img class="is-rounded" width="100" height="100" src="../assets/me.jpg" alt="LAU ZHAO LIN">
+          </figure>
+        </div>
+        <div class="column is-8">
+          <p class="subtitle has-text-centered">Jack of all trades, master of "some"</p>
+          <p class="has-text-justified">
+            Web developer with more than <b>3 years</b> of well-rounded experience with a diploma in the field of <b>Computer Science</b>, extensive knowledge of modern Web techniques and love for <b>Milo</b>. Looking for an opportunity to work and upgrade, as well as being involved in an organization that believes in gaining a competitive edge and giving back to the community.
+          </p>
+        </div>
+      </div>
     </div>
   </section>
 </template>
