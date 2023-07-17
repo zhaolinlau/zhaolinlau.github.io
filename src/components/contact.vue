20427212f9bbf526beaf73ec25007f8b8570d8d0
Zhaolin Lau <zhaolinlau@gmail.com>
2023-07-18 01:30:54 +0800

    add confact function

--- src/components/contact.vue
+++ src/components/contact.vue
@@ -1,28 +1,33 @@
 <template>
 	<section class="section" id="contact">
 		<div class="container">
-			<p class="title has-text-centered mt-3">Contact Me(In progess)</p>
+			<p class="title has-text-centered mt-3">Contact Me</p>
 			<div class="columns">
 				<div class="column is-6 is-offset-3">
-					<form class="box" action="" method="post">
-						<o-field label="Subject">
-							<o-input required hasCounter></o-input>
-						</o-field>
+					<form class="box" action="https://formsubmit.co/c771b3ea7ca4ec5d9aa6e7621c42027e" method="post">
 
-						<o-field label="Name">
+						<input type="hidden" name="_captcha" value="false">
+
+						<input type="hidden" name="_next" value="https://zhaolinlau.vercel.app/">
+
+						<o-field label="Subject">
 							<o-input required></o-input>
 						</o-field>
 
+						<o-field label="Name">
+							<o-input name="name" required></o-input>
+						</o-field>
+
 						<o-field label="Email">
-							<o-input type="email" required></o-input>
+							<o-input type="email" name="email" required></o-input>
 						</o-field>
 
 						<o-field label="Message">
-							<o-input type="textarea" required></o-input>
+							<o-input type="textarea" name="message" required></o-input>
 						</o-field>
 
 						<o-field grouped>
-							<o-button nativeType="submit" variant="primary" rounded disabled>Send</o-button>
+							<o-button nativeType="submit" variant="primary" rounded>Send</o-button>
 							<o-button nativeType="reset" rounded>Reset</o-button>
 						</o-field>
 					</form>
