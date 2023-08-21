eaf634d2e86d900bc56c6978afcbff9822063655
Zhaolin Lau <zhaolinlau@gmail.com>
2023-08-21 18:18:59 +0800

    add mlbb:roct project; add uxhmac project

--- pages/index.vue
+++ pages/index.vue
@@ -223,6 +223,95 @@ function submitForm(event) {
 			</p>
 
 			<div class="columns is-multiline is-centered">
+
+				<div class="column is-4">
+					<div class="card">
+						<header class="card-header">
+							<p class="card-header-title">
+								<span>UMPSA x Huawei Mobile App Competition</span>
+							</p>
+						</header>
+						<div class="card-content">
+							<figure class="image">
+								<nuxt-img src="img/uxhmac.png" alt="uxhmac.png" />
+							</figure>
+						</div>
+						<footer class="card-footer">
+							<a href="https://uxhmac.vercel.app/" class="card-footer-item" target="_blank">
+								<span class="icon-text">
+									<o-icon icon="eye"></o-icon>
+									<span>Preview</span>
+								</span>
+							</a>
+							<div class="card-footer-item is-unselectable">
+								<span class="icon-text">
+									<o-icon icon="lock"></o-icon>
+									<span>Private</span>
+								</span>
+							</div>
+						</footer>
+					</div>
+				</div>
+
+				<div class="column is-4">
+					<div class="card">
+						<header class="card-header">
+							<p class="card-header-title">
+								<span>MLBB: Rise of Champions Tournament</span>
+							</p>
+						</header>
+						<div class="card-content">
+							<figure class="image">
+								<nuxt-img src="img/mlbb-roct.png" alt="mlbb-roct.png" />
+							</figure>
+						</div>
+						<footer class="card-footer">
+							<a href="https://umpsaesport.vercel.app/" class="card-footer-item" target="_blank">
+								<span class="icon-text">
+									<o-icon icon="eye"></o-icon>
+									<span>Preview</span>
+								</span>
+							</a>
+							<div class="card-footer-item is-unselectable">
+								<span class="icon-text">
+									<o-icon icon="lock"></o-icon>
+									<span>Private</span>
+								</span>
+							</div>
+						</footer>
+					</div>
+				</div>
+
+				<div class="column is-4">
+					<div class="card">
+						<header class="card-header">
+							<p class="card-header-title">
+								<span>Huawei Open Day: Intelligent World 2030</span>
+							</p>
+						</header>
+						<div class="card-content">
+							<figure class="image">
+								<nuxt-img src="img/huawei-intelligent-world-2030.png" alt="huawei-intelligent-world-2030.png" />
+							</figure>
+						</div>
+						<footer class="card-footer">
+							<a href="https://huawei-intelligent-world-2030.vercel.app/" class="card-footer-item" target="_blank">
+								<span class="icon-text">
+									<o-icon icon="eye"></o-icon>
+									<span>Preview</span>
+								</span>
+							</a>
+							<a href="https://github.com/zhaolinlau/huawei-intelligent-world-2030" class="card-footer-item"
+								target="_blank">
+								<span class="icon-text">
+									<o-icon icon="github"></o-icon>
+									<span>Source Code</span>
+								</span>
+							</a>
+						</footer>
+					</div>
+				</div>
+
 				<div class="column is-4">
 					<div class="card">
 						<header class="card-header">
@@ -281,35 +370,6 @@ function submitForm(event) {
 					</div>
 				</div>
 
-				<div class="column is-4">
-					<div class="card">
-						<header class="card-header">
-							<p class="card-header-title">
-								<span>Huawei Open Day: Intelligent World 2030</span>
-							</p>
-						</header>
-						<div class="card-content">
-							<figure class="image">
-								<nuxt-img src="img/huawei-intelligent-world-2030.png" alt="huawei-intelligent-world-2030.png" />
-							</figure>
-						</div>
-						<footer class="card-footer">
-							<a href="https://huawei-intelligent-world-2030.vercel.app/" class="card-footer-item" target="_blank">
-								<span class="icon-text">
-									<o-icon icon="eye"></o-icon>
-									<span>Preview</span>
-								</span>
-							</a>
-							<a href="https://github.com/zhaolinlau/huawei-intelligent-world-2030" class="card-footer-item"
-								target="_blank">
-								<span class="icon-text">
-									<o-icon icon="github"></o-icon>
-									<span>Source Code</span>
-								</span>
-							</a>
-						</footer>
-					</div>
-				</div>
 			</div>
 		</div>
 	</section>
