ec6e9b0eac3dcbc7db0137ef3ea841a7e75daecb
Zhaolin Lau <zhaolinlau@gmail.com>
2023-08-26 19:10:43 +0800

    add back to top component

---
+++ components/BackTop.vue
@@ -0,0 +1,33 @@
+<script setup>
+const showButton = ref(false);
+
+onMounted(() => {
+	window.addEventListener('scroll', handleScroll);
+})
+
+function handleScroll() {
+	showButton.value = window.scrollY > 250;
+}
+
+function scrollToTop() {
+	window.scrollTo({ top: 0, behavior: 'smooth' });
+}
+
+onUnmounted(() => {
+	window.removeEventListener('scroll', handleScroll);
+});
+</script>
+
+<template>
+	<o-button class="scrolltotop animate__animated animate__fadeIn" v-if="showButton" @click="scrollToTop" variant="dark"
+		icon-left="arrow-up"></o-button>
+</template>
+
+<style>
+.scrolltotop {
+	position: fixed;
+	bottom: 20px;
+	right: 20px;
+	z-index: 99;
+}
+</style>
