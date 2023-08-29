b38cb1798aee0e7f7bfd0d6ea884ada8832d4306
Zhaolin Lau <zhaolinlau@gmail.com>
2023-08-29 22:41:36 +0800

    update license

--- components/Footer.vue
+++ components/Footer.vue
@@ -2,7 +2,10 @@
 	<footer class="footer mt-3">
 		<div class="content has-text-centered">
 			<p>
-				The website content is licensed
+				Source code licensed under
+				<a href="https://www.gnu.org/licenses/gpl-3.0.html">GNU GPLv3</a>.
+				<br>
+				Website content licensed under
 				<a href="http://creativecommons.org/licenses/by-nc-sa/4.0/" target="_blank">CC BY-NC-SA 4.0</a>.
 			</p>
 		</div>
