c321b978bc7fffa0ad2699ee404cf28ad6a56505
Zhaolin Lau <zhaolinlau@gmail.com>
2023-08-31 16:42:25 +0800

    optimize social button & change to fontawesome icon

--- plugins/oruga.ts
+++ plugins/oruga.ts
@@ -3,6 +3,7 @@ import { bulmaConfig } from "@oruga-ui/theme-bulma";
 import "@oruga-ui/theme-bulma/dist/bulma.css";
 import "@mdi/font/css/materialdesignicons.css";
 import "animate.css/animate.css";
+import "@fortawesome/fontawesome-free/css/all.css";
 
 export default defineNuxtPlugin((nuxtApp) => {
 	nuxtApp.vueApp.use(Oruga, bulmaConfig);
