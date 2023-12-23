d1242645490e73d4ea9dde445a9bb9781c275915
Zhaolin Lau <zhaolinlau@gmail.com>
2023-12-23 15:55:17 +0800

    enable typescript takeover mode

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,7 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
-
+	typescript: {
+		shim: false,
+	},
 	modules: ["@nuxt/image"],
 
 	plugins: ["plugins/oruga.ts"],
