e4c07e635b18d7787a64ce6a61c45d8481d1b23c
Zhaolin Lau <zhaolinlau@gmail.com>
2024-03-07 15:18:38 +0800

    enable typescript shim

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,9 +1,6 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: false },
-	typescript: {
-		shim: false,
-	},
 	modules: [
 		"nuxt-icon",
 		"@nuxt/image",
