c9f57b0620098ebce6c6c36fa81f789db20857a1
Zhaolin Lau <zhaolinlau@gmail.com>
2024-06-25 01:32:21 +0800

    add bulma css

--- plugins/oruga.js
+++
@@ -1,9 +0,0 @@
-import Oruga from "@oruga-ui/oruga-next";
-import { bulmaConfig } from "@oruga-ui/theme-bulma";
-import "@oruga-ui/theme-bulma/dist/bulma.css";
-import "@mdi/font/css/materialdesignicons.css";
-import "animate.css/animate.css";
-
-export default defineNuxtPlugin((nuxtApp) => {
-	nuxtApp.vueApp.use(Oruga, bulmaConfig);
-});
