a2220e9792f9d87c9af4d6724be762b2eed9490c
Zhaolin Lau <zhaolinlau@gmail.com>
2024-07-24 16:09:51 +0800

    set social icon size to 1.2em

--- components/SocialButtons.vue
+++ components/SocialButtons.vue
@@ -36,7 +36,7 @@ const socials = [
 <template>
 	<div class="buttons">
 		<NuxtLink :class="`button is-${social.variant}`" :to="social.to" target="_blank" v-for="social in socials">
-			<Icon :name="social.icon" />
+			<Icon :name="social.icon" size="1.2em" />
 			<span>{{ social.title }}</span>
 		</NuxtLink>
 	</div>
