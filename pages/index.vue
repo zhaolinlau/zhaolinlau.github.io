3a8899b84500eefa5387748298f6748c7abea152
Zhaolin Lau <zhaolinlau@gmail.com>
2024-08-24 15:46:35 +0800

    update intro

--- pages/index.vue
+++ pages/index.vue
@@ -43,7 +43,7 @@ const expYear = date.getFullYear() - 2021;
 				<div class="column is-8">
 					<p class="subtitle has-text-centered">Jack of all trades, master of "some"</p>
 					<p class="has-text-justified">
-						Web developer with more than <b>{{ expYear }} years</b> of well-rounded experience with a
+						Web developer with more than <b>{{ expYear }} years</b> of well-rounded hands-on experience with a
 						diploma in the field of
 						<b>Computer Science</b>, extensive knowledge of modern Web techniques and love for <b>Lemon Tea</b>. Looking
 						for
