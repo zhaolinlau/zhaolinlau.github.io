3c8bef2fe0ea871b65250d8895a240826d050fb0
Zhaolin Lau <zhaolinlau@gmail.com>
2025-01-31 00:16:37 +0800

    add mobile breakpoint for social buttons

--- components/SocialButtons.vue
+++ components/SocialButtons.vue
@@ -4,10 +4,18 @@ const socials = useSocial()
 
 <template>
 	<div class="mt-3">
-		<Button variant="outline" as-child v-for="(social, index) in socials" :key="social">
-			<NuxtLink :class="`${index != 0 ? 'ml-3' : ''}`" :to="social.to" target="_blank">
-				<Icon :name="social.icon" /> {{ social.title }}
-			</NuxtLink>
-		</Button>
+		<template v-for="(social, index) in socials" :key="social">
+			<Button variant="outline" as-child class="hidden lg:inline-block">
+				<NuxtLink :class="`${index != 0 ? 'ml-3' : ''}`" :to="social.to" target="_blank">
+					<Icon :name="social.icon" /> {{ social.title }}
+				</NuxtLink>
+			</Button>
+
+			<Button variant="outline" as-child class="lg:hidden" size="icon">
+				<NuxtLink :class="`${index != 0 ? 'ml-3' : ''}`" :to="social.to" target="_blank">
+					<Icon :name="social.icon" />
+				</NuxtLink>
+			</Button>
+		</template>
 	</div>
 </template>
