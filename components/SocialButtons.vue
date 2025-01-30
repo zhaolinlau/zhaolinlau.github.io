75bf2cf726491727ff9317568537a08ae3a4560a
Zhaolin Lau <zhaolinlau@gmail.com>
2025-01-30 15:27:34 +0800

    rewrite social buttons

--- components/SocialButtons.vue
+++ components/SocialButtons.vue
@@ -3,10 +3,11 @@ const socials = useSocial()
 </script>
 
 <template>
-	<div class="buttons">
-		<NuxtLink :class="`button is-${social.variant}`" :to="social.to" target="_blank" v-for="social in socials">
-			<Icon :name="social.icon" />
-			<span>{{ social.title }}</span>
-		</NuxtLink>
+	<div class="mt-3">
+		<Button variant="outline" as-child v-for="(social, index) in socials" :key="social">
+			<NuxtLink :class="`${index != 0 ? 'ml-3' : ''}`" :to="social.to" target="_blank">
+				<Icon :name="social.icon" /> {{ social.title }}
+			</NuxtLink>
+		</Button>
 	</div>
 </template>
