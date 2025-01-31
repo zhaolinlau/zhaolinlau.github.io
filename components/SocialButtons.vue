d240e5ad0451a4d2b06dcaf13f357f02b0b32308
Zhaolin Lau <zhaolinlau@gmail.com>
2025-01-31 21:05:28 +0800

    set all breakpoint to have ml-3 for social buttons

--- components/SocialButtons.vue
+++ components/SocialButtons.vue
@@ -6,7 +6,7 @@ const socials = useSocial()
 	<div>
 		<template v-for="(social, index) in socials" :key="social">
 			<Button variant="outline" as-child>
-				<NuxtLink :class="`${index != 0 ? 'lg:ml-3' : ''}`" :to="social.to" target="_blank">
+				<NuxtLink :class="`${index != 0 ? 'ml-3' : ''}`" :to="social.to" target="_blank">
 					<Icon :name="social.icon" :class="social.color" /><span class="hidden lg:inline-flex"> {{
 						social.title }}</span>
 				</NuxtLink>
